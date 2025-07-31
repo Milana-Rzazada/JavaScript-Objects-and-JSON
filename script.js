edc113c8da8961810ef63eacb237302cec826ef4
Milana-Rzazade <71l1c0h@code.edu.az>
2025-07-31 15:21:07 -0400

    Create studen object

---
+++ script.js
@@ -0,0 +1,22 @@
+//part 1
+console.log("PART 1 STARTED:");
+const student = {
+    name: "Milana",
+    age: "24",
+    enrolled: true,
+    courses: ["Web design", "Javascript's fundamentals", "Algorithms aand Problem Solving", "Database Concepts and Design", "Operating systems"],
+    displayInfo: function() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Age: ${this.age}`);
+        console.log(`Enrolled: ${this.enrolled ? "Yes": "No"}`);
+        console.log(`Courses: ${this.courses.join(",")}`);
+    }
+};
+
+console.log("Nmae: ", student.name);
+console.log("Age: ", student.age);
+
+student.displayInfo();
+console.log("PART 1 ENDED:");
+
+
